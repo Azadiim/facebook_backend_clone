33afb10d26adb4c62133bd0f7d314506e2d94410
Azad <AzadAmirir88@gmail.com>
2024-03-10 00:42:02 +0330

    the first commit

--- middlewares/imageUpload.js
+++ middlewares/imageUpload.js
@@ -1,8 +1,30 @@
+import fs from "fs";
 const imageUpload = async (req, res, next) => {
   try {
+    if (!req.files || Object.values(req.files).flat().length === 0) {
+      res.status(400).json({ message: "No files were uploaded!" });
+    }
+    let files = Object.values(req.files).flat();
+    files.forEach((file) => {
+      if (
+        file.mimetype !== "image/jpeg" &&
+        file.mimetype !== "image/png" &&
+        file.mimetype !== "image/gif" &&
+        file.mimetype !== "image/webp"
+      ) {
+        removeTemp(file.tempFilePath);
+        return res.status(400).json({ message: "Unsupported format!" });
+      }
+    });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
+};
+
+const removeTemp = (path) => {
+  fs.unlink(path, (err) => {
+    if (err) throw err;
+  });
 };
 
 export default imageUpload;
