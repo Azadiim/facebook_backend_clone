e2945c23fb6f8a9c332158b8690fa54500f484e6
Azad <AzadAmirir88@gmail.com>
2024-03-11 00:02:40 +0330

    adding the cloudinary function for uploading images!

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,9 +1,50 @@
+import cloudinary from "cloudinary";
+import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
 const uploadImages = async (req, res) => {
+  const { path } = req.body;
+  let files = Object.values(req.files).flat();
+  let images = [];
+  for (const file of files) {
+    const url = await uploadToCloudinary(file, path);
+    images.push(url);
+    removeTemp(file.tempFilePath);
+  }
+  res.json(images);
+
   try {
-    res.json("welcome from image upload");
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
+};
+
+const uploadToCloudinary = async (file, path) => {
+  return new Promise((resolve) => {
+    cloudinary.v2.uploader.upload(
+      file.tempFilePath,
+      { folder: path },
+      (err, res) => {
+        if (err) {
+          removeTemp(file.tempFilePath);
+          return res.status(400).json({ message: "Upload images failed." });
+        }
+        resolve({ url: res.secure_url });
+      }
+    );
+  });
+};
+
+const removeTemp = (path) => {
+  fs.unlink(path, (err) => {
+    if (err) throw err;
+  });
 };
 
 export { uploadImages };
