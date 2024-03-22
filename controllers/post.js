0cd015046c4bbcc55ce44e762423426b4bede373
Azad <AzadAmirir88@gmail.com>
2024-03-22 18:09:00 +0330

    adding some changes for more security

--- controllers/post.js
+++ controllers/post.js
@@ -4,7 +4,7 @@ const createPost = async (req, res) => {
     const post = await new Post(req.body).save();
     res.json(post);
   } catch (error) {
-    return res.status(500).json({ message: error.message });
+    return res.status(500).json({ message: 'Invalid Authentication' });
   }
 };
 
--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { uploadImages } from "../controllers/upload.js";
 import imageUpload from "../middlewares/imageUpload.js";
+import { userAuth } from "../middlewares/auth.js";
 
 const router = express.Router();
 
